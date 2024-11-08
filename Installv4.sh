f2d9458c1442ab77be3124c0c592a5e83748e9ab
RiiJustHuman <fakhriajafakhriii@gmail.com>
2024-11-09 00:44:19 +0700

    Update Installv4.sh

--- Installv4.sh
+++ Installv4.sh
@@ -976,8 +976,8 @@ read -p "Masukkan nama lokasi: " location_name
 read -p "Masukkan deskripsi lokasi: " location_description
 read -p "Masukkan domain: " domain
 read -p "Masukkan nama node: " node_name
-read -p "Masukkan RAM (dalam MB): " ram
-read -p "Masukkan jumlah maksimum disk space (dalam MB): " disk_space
+read -p "Masukkan RAM : " ram
+read -p "Masukkan jumlah maksimum disk space: " disk_space
 read -p "Masukkan Locid: " locid
 
 # Ubah ke direktori pterodactyl
